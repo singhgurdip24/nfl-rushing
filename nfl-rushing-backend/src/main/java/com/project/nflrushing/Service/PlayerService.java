ed4c1c473e889bc7305fd947b3a4a0e80bec5bb7
Gurdip Singh <singh.gurdip24@gmail.com>
2020-12-13 19:00:40 -0500

    added dockerfile

--- nfl-rushing-backend/src/main/java/com/project/nflrushing/Service/PlayerService.java
+++ nfl-rushing-backend/src/main/java/com/project/nflrushing/Service/PlayerService.java
@@ -8,7 +8,6 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
