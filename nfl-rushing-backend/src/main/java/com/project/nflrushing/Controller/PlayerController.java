23c53e6f5bc90d0fc3c804a8ae76949ec0a85393
Gurdip Singh <singh.gurdip24@gmail.com>
2020-12-15 17:46:18 -0500

    local development set up

--- nfl-rushing-backend/src/main/java/com/project/nflrushing/Controller/PlayerController.java
+++ nfl-rushing-backend/src/main/java/com/project/nflrushing/Controller/PlayerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class PlayerController {
 
--- nfl-rushing-frontend/src/constants/index.js
+++ nfl-rushing-frontend/src/constants/index.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = process.env.NFL_APP_API_BASE_URL || 'http://localhost/api';
+//export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'http://localhost/api';
 
 export const COLUMN_NAME = [
     {
